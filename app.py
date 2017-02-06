4e266c716c2c5476e5b6156fe75e6254a63058c0
melipelo <melo.perez91@gmail.com>
2017-02-06 13:25:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -28,15 +28,15 @@ def webhook():
     return r
 
 def makeWebhookResult(req):
-    if req.get("result").get("action") != "shipping.cost":
+    if req.get("result").get("action") != "productos.info":
         return {}
     result = req.get("result")
     parameters = result.get("parameters")
-    zone = parameters.get("shipping-zone")
+    cliente = parameters.get("tipo_cliente")
 
-    cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
+    #cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
 
-    speech = "The cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
+    speech = "Buscando productos para " + cliente
 
     print("Response:")
     print(speech)
