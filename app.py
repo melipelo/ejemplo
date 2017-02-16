e61cf10e6caae31bcdce6d248186feb85172a7cf
melipelo <melo.perez91@gmail.com>
2017-02-16 10:21:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -35,11 +35,10 @@ def makeWebhookResult(req):
 	elif req.get("result").get("action") == "producto.info":
         	producto = parameters.get("producto")
 		if(producto == "salud"):
-			#url = "https://lab.segurossura.com.co/paginas/salud/inicio.aspx"
-			url = "https://www.sura.com/saludsura/sections/seguros-de-salud.aspx"
+			url = "https://lab.segurossura.com.co/paginas/salud/inicio.aspx"
 			r = urllib2.urlopen(url).read()
 			soup = BeautifulSoup(r, 'html.parser')
-			contenido = soup.find_all("div",class_="fondoRazones ")
+			contenido = soup.find_all("div",class_="col-sm-12 cp ")
 			speech = contenido[0]
 		else:
         		speech = "Buscando informacion del producto " + producto
